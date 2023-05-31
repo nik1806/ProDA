96f8cff180915a1155f7f716a7e87bac3753606e
nik1806 <nikhil.paliwal14@gmail.com>
2023-05-31 13:49:28 +0200

    Add: Deeplabv2 from DASS and run.sh to execute

---
+++ run.sh
@@ -0,0 +1,6 @@
+python generate_pseudo_label.py \
+    --name gta2citylabv2_stage1Denoise --flip \
+    --resume_path pretrained/gta2citylabv2_stage1Denoise/gta5_deepv2_trained_dass51.pth \
+    --no_droplast
+
+    # --resume_path logs/gta2citylabv2_stage1Denoise/from_gta5_to_cityscapes_on_deeplabv2_best_model.pkl \
